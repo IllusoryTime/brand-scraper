ae12618168f4f15a6b18597284358d3d84c7f0a8
Afzal Hussain <afzalhussain@iut-dhaka.edu>
2022-08-01 22:27:16 +0600

    Add WebPage and ImageMetadata model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import ImageMetadata, WebPage
+
+
+class WebPageAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in WebPage._meta.fields]
+
+
+class ImageMetadataAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in ImageMetadata._meta.fields]
+
+
+admin.site.register(WebPage, WebPageAdmin)
+admin.site.register(ImageMetadata, ImageMetadataAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
+import uuid
+
+from django.core.validators import URLValidator
 from django.db import models
 
-# Create your models here.
+
+class WebPage(models.Model):
+    id = models.BigAutoField(
+        auto_created=True, primary_key=True, verbose_name='ID')
+    url = models.URLField(unique=True)
+
+
+class ImageMetadata(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    web_page = models.ForeignKey(
+        to=WebPage, on_delete=models.SET_NULL, null=True),
+    image_url = models.URLField()
+    file_name = models.CharField(max_length=255)
+    height = models.FloatField()
+    width = models.FloatField()
+    scrape_date = models.DateField()
+    file_size = models.FloatField()
