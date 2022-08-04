12fbbebb833b7c25672c9212151cc02401195b92
Afzal Hussain <afzalhussain@iut-dhaka.edu>
2022-08-05 00:26:45 +0600

    Update models fields and their corresponding serializer

--- api/models.py
+++ api/models.py
@@ -4,18 +4,15 @@ from django.db import models
 
 
 class WebPage(models.Model):
-    id = models.BigAutoField(
-        auto_created=True, primary_key=True, verbose_name='ID')
+    id = models.BigAutoField(primary_key=True)
     url = models.URLField(unique=True)
 
 
 class ImageMetadata(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    web_page = models.ForeignKey(
-        to=WebPage, on_delete=models.SET_NULL, null=True)
-    image_url = models.URLField()
+    web_page = models.ForeignKey(to=WebPage, on_delete=models.SET_NULL, null=True)
     file_name = models.CharField(max_length=255)
-    height = models.FloatField()
-    width = models.FloatField()
+    height = models.IntegerField()
+    width = models.IntegerField()
     scrape_date = models.DateField()
-    file_size = models.FloatField()
+    file_size = models.DecimalField(max_digits=8, decimal_places=2, )
--- api/serializers.py
+++ api/serializers.py
@@ -8,15 +8,22 @@ class WebPageSerializer(serializers.ModelSerializer):
     class Meta:
         model = WebPage
         fields = "__all__"
+        extra_kwargs = {
+            'url': {'validators': []}
+        }
 
 
 class ImageMetadataSerializer(serializers.ModelSerializer):
     web_page = serializers.CharField(source='web_page.url')
-    image_url = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()  # TODO: Come back to here
 
     class Meta:
         model = ImageMetadata
         fields = "__all__"
+        extra_kwargs = {
+            'file_size': {'coerce_to_string': False}
+        }
 
-    def get_image_url(self, obj):
+    @staticmethod
+    def get_image_url(obj):
         return static(obj.file_name)
