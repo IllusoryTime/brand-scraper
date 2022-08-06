ba5a24404e9aca0751d0f5d17ef7af0ee4f9d9b9
Afzal Hussain <afzalhussain@iut-dhaka.edu>
2022-08-06 20:13:08 +0600

    Refactor image metadata API
    Use ReadOnlyModelViewSet and aggregate retrieve and list API under a single class.

--- api/views.py
+++ api/views.py
@@ -1,13 +1,15 @@
 from urllib.parse import urlparse
 
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, status
+from django.templatetags.static import static
+from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
 from scrapyd_api import ScrapydAPI
 from url_normalize import url_normalize
 
 from api.models import ImageMetadata
 from api.serializers import WebPageSerializer, ImageMetadataSerializer
+from api.utils import is_valid_uuid
 
 scrapyd = ScrapydAPI('http://localhost:6800')
 
@@ -33,13 +35,31 @@ class ImageMetadataListAPIView(generics.ListAPIView):
     serializer_class = ImageMetadataSerializer
 
 
-class ImageMetadataByIDRetrieveAPIView(generics.RetrieveAPIView):
+class ImageMetadataViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ImageMetadata.objects.all()
-    serializer_class = ImageMetadataSerializer
 
+    def list(self, request, *args, **kwargs):
+        """
+        If no url query parameter is passed we are sending all the records.
+        Because in standard GET API data should always be sent. Query parameters are used for filtering.
+        """
 
-class ImageMetadataByURLRetrieveAPIView(generics.RetrieveAPIView):
-    serializer_class = ImageMetadataSerializer
+        queryset = self.queryset
+        url = request.GET.get('url', None)
 
-    def get_object(self):
-        return get_object_or_404(ImageMetadata, web_page__url__iexact=self.kwargs.get('web_page'))
+        if url:
+            normalized_url = url_normalize(url)
+            queryset = ImageMetadata.objects.filter(web_page__url__iexact=normalized_url)
+
+        serializer = ImageMetadataSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+
+        if not is_valid_uuid(pk):
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        image_metadata = get_object_or_404(ImageMetadata, pk=pk)
+        serializer = ImageMetadataSerializer(image_metadata)
+        return Response(serializer.data)
--- brandscraper/urls.py
+++ brandscraper/urls.py
@@ -6,7 +6,6 @@ from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('scrape_image/', views.ImageScrapperAPIView.as_view()),
-    path('image/metadata/all', views.ImageMetadataListAPIView.as_view()),
-    path('image/metadata/id/<uuid:pk>', views.ImageMetadataByIDRetrieveAPIView.as_view()),
-    path('image/metadata/url/<str:web_page>', views.ImageMetadataByURLRetrieveAPIView.as_view()),
+    path('image/metadata/', views.ImageMetadataViewSet.as_view({'get': 'list'})),
+    path('image/metadata/<str:pk>', views.ImageMetadataViewSet.as_view({'get': 'retrieve'})),
 ]
