66efc6988219cbfa20b7ad7cacceb5b52522f200
Afzal Hussain <afzalhussain@iut-dhaka.edu>
2022-08-07 02:59:25 +0600

    Redirect homepage to scrape 'image_page' view as there is no view for the homepage

--- brandscraper/urls.py
+++ brandscraper/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 from api import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='scrape_image/')),
     path('admin/', admin.site.urls),
     path('scrape_image/', views.ImageScraperViewSet.as_view({'post': 'create'})),
     path('image/', views.ImageViewSet.as_view({'get': 'list'})),
